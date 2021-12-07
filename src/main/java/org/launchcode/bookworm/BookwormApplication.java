189d4b6ba6be32748d96c97c8f391723ad7373fc
iainshaw <iainxiao@gmail.com>
2021-12-06 19:52:52 -0600

    Initial commit

---
+++ src/main/java/org/launchcode/bookworm/BookwormApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.bookworm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookwormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/bookworm/BookwormApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.bookworm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookwormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
