9f2e631067e00d80605dc8beee268d3e2af5bbf9
Katie Dessain <JillSandwich@example.com>
2022-01-11 20:01:03 -0600

    added a UserController class

---
+++ src/main/java/org/launchcode/bookworm/controllers/UserController.java
@@ -0,0 +1,59 @@
+package org.launchcode.bookworm.controllers;
+
+import org.launchcode.bookworm.User;
+import org.launchcode.bookworm.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("")
+    public String index(Model model){
+        model.addAttribute("title", "All Users");
+        model.addAttribute("users", userRepository.findAll());
+        return "users/index";
+    }
+
+    @GetMapping("add")
+    public String displayAddUsersForm(Model model) {
+        model.addAttribute(new User());
+        return "users/add";
+    }
+
+    @PostMapping("add")
+    public String processAddUserForm(@ModelAttribute @Valid User newUser,
+                                         Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            return "users/add";
+        }
+
+        userRepository.save(newUser);
+        return "redirect:";
+    }
+
+    @GetMapping("view/{employerId}")
+    public String displayViewUser(Model model, @PathVariable int userId) {
+
+        Optional optUser = userRepository.findById(userId);
+        if (optUser.isPresent()) {
+            User user = (User) optUser.get();
+            model.addAttribute("user", user);
+            return "users/view";
+        } else {
+            return "redirect:../";
+        }
+    }
+
+}
